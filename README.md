bac1767cb2657b93ab04190c95ad34e85168cd18
ZikXewen <datasup12@gmail.com>
2022-06-03 06:22:35 +0700

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# [Rustlings](https://github.com/rust-lang/rustlings) 4.7.1 Answer
+
+Might try keep up to date with upcoming Rustlings versions.
+
+By ZikXewen.
---
+++ enums/enums2.rs
@@ -0,0 +1,29 @@
+// enums2.rs
+// Make me compile! Execute `rustlings hint enums2` for hints!
+
+#[derive(Debug)]
+enum Message {
+    Move { x: u8, y: u8 },
+    Echo(String),
+    ChangeColor(u8, u8, u8),
+    Quit,
+}
+
+impl Message {
+    fn call(&self) {
+        println!("{:?}", &self);
+    }
+}
+
+fn main() {
+    let messages = [
+        Message::Move { x: 10, y: 30 },
+        Message::Echo(String::from("hello world")),
+        Message::ChangeColor(200, 255, 255),
+        Message::Quit,
+    ];
+
+    for message in &messages {
+        message.call();
+    }
+}
